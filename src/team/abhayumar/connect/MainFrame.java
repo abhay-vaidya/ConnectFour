d65ddd00fb2602c3cd8a19dfa7604d3eea585d68
323977454 <323977454@C-C80RCX1.tdsb.on.ca>
2016-03-11 13:22:38 -0500

    Added grid square background.

--- src/team/abhayumar/connect/MainFrame.java
+++ src/team/abhayumar/connect/MainFrame.java
@@ -12,4 +12,11 @@ public class MainFrame extends JFrame {
 		
 		getContentPane().setBackground(Color.decode("#2196F3"));
 	}
+
+public void paint(Graphics g) {
+    g.setColor(Color.GRAY);
+    g.fillRect (100, 100, 500, 500);
+    getContentPane().setBackground(Color.decode("#2196F3"));
+  }
 }
+
