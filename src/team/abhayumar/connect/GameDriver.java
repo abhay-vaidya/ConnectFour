258e590448116a1a329511e53b6e4150e61600ab
Abhay Vaidya <abhay6547@gmail.com>
2016-03-10 23:01:58 -0500

    Set up main frame as separate class for modularity.

--- src/team/abhayumar/connect/GameDriver.java
+++ src/team/abhayumar/connect/GameDriver.java
@@ -1,9 +1,22 @@
 package team.abhayumar.connect;
+import javax.swing.*;
+
+import team.abhayumar.connect.MainFrame;
 
 public class GameDriver {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				JFrame frame = new MainFrame("Connect Four");
+				// ImageIcon img = new ImageIcon("");
+				// frame.setIconImage(img.getImage());
+				frame.setSize(700, 700);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+				frame.setLocationRelativeTo(null);
+			}
+		});
 	}
 
 }
---
+++ src/team/abhayumar/connect/MainFrame.java
@@ -0,0 +1,14 @@
+package team.abhayumar.connect;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	public MainFrame(String title){
+		super(title);
+		
+		getContentPane().setBackground(Color.decode("#2196F3"));
+	}
+
+}
