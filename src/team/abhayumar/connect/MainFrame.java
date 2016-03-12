c34d1d22e68f31a41a97f8b2481a8f177d7fe9ff
Abhay Vaidya <abhay6547@gmail.com>
2016-03-12 14:27:06 -0500

    Cleanup and commenting for future reference.

--- src/team/abhayumar/connect/MainFrame.java
+++ src/team/abhayumar/connect/MainFrame.java
@@ -2,37 +2,40 @@ package team.abhayumar.connect;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridBagLayout;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.OverlayLayout;
 import javax.swing.JButton;
-import java.awt.CardLayout;
 
 public class MainFrame extends JFrame {
 	public MainFrame(String title){
 		super(title);
 		
+		// Set temporary frame layout
 		getContentPane().setLayout(new BorderLayout());
 
+		// Overlay panel to hold all other panels
 		JPanel mainMenuPanel = new JPanel();
-		JPanel buttons = new JPanel();
+		mainMenuPanel.setLayout(new OverlayLayout(mainMenuPanel));
+		
+		// Panels to be overlaid
 		JPanel background = new JPanel();
+		JPanel graphics = new JPanel();
+		JPanel buttons = new JPanel();
+
+		// Test item
 		JButton btnTestButton = new JButton("TEST BUTTON");
 		
-		mainMenuPanel.setLayout(new OverlayLayout(mainMenuPanel));
-
+		// Panel attributes
 		buttons.setOpaque(false);
 		buttons.add(btnTestButton);
 		
 		background.setBackground(Color.decode("#2196F3"));
 		background.setPreferredSize(getMaximumSize());
 		
+		// Add all panels into frame
 		mainMenuPanel.add(buttons);
 		mainMenuPanel.add(background);
 		getContentPane().add(mainMenuPanel);
-
 	}
 }
