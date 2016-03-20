a48a2fc3db80e180b1a9ec48ac637ecbbf8447a1
Umar Ahmed <umar.ahmed1998@gmail.com>
2016-03-20 17:03:39 -0400

    Added MouseListener, removed buttons

---
+++ src/team/abhayumar/connect/C4Panel.java
@@ -0,0 +1,45 @@
+package team.abhayumar.connect;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class C4Panel extends JPanel implements MouseListener {
+	
+	public C4Panel() {
+		addMouseListener(this);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		System.out.println("x:" + String.valueOf(e.getX()) + " y: " + String.valueOf(e.getY()));
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/team/abhayumar/connect/MainFrame.java
+++ src/team/abhayumar/connect/MainFrame.java
@@ -1,25 +1,16 @@
 package team.abhayumar.connect;
 
 import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.Color;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
-
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.OverlayLayout;
-import javax.swing.SwingConstants;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
@@ -27,28 +18,25 @@ public class MainFrame extends JFrame {
 	private static Game newGame;
 	
 	// Panels to be merged
-	private HashMap<String, JPanel> panels = new HashMap<String, JPanel>();
+	private HashMap<String, C4Panel> panels = new HashMap<String, C4Panel>();
 	// Colors
 	private static Color[] colors = {null, new Color(255,0,0), new Color(255,255,0)};
+	
+	
 	
 	public MainFrame(String title){
 		this.setTitle(title);
 		
 		// Create panels and store in Map for easy access
-		panels.put("background", new JPanel());
-		panels.put("buttons", new JPanel());
-		panels.put("graphics", new JPanel());
+		panels.put("background", new C4Panel());
+		panels.put("graphics", new C4Panel());
 		
 
 		// Create overlay panel to hold all other panels
-		JPanel mainMenuPanel = new JPanel();
+		C4Panel mainMenuPanel = new C4Panel();
 		mainMenuPanel.setLayout(new OverlayLayout(mainMenuPanel));
-		
-		
-		// Temporary button layout
-		GridLayout gl_buttons = new GridLayout(6, 7, 0, 0);
-		panels.get("buttons").setLayout(gl_buttons);
-		
+				
+			
 		// Board image
 		BufferedImage myPicture = null;
 		try {
@@ -62,21 +50,16 @@ public class MainFrame extends JFrame {
 		
 		// Set transparency
 		panels.get("graphics").setOpaque(false);
-		panels.get("buttons").setOpaque(true);
 
 		// Background
 		panels.get("background").setBackground(Color.decode("#FFE082"));
 		panels.get("background").setPreferredSize(getMaximumSize());
 
-		// Test button
-		for (int i=0; i<(Game.ROWS * Game.COLUMNS); i++) {
-			panels.get("buttons").add(new Button("Hi"));
-		}
+		
 		
 		
 		mainMenuPanel.add(panels.get("graphics"));
-		mainMenuPanel.add(panels.get("buttons"));
-		//mainMenuPanel.add(panels.get("background"));
+		mainMenuPanel.add(panels.get("background"));
 
 		getContentPane().add(mainMenuPanel);
 		
@@ -127,5 +110,7 @@ public class MainFrame extends JFrame {
 		
 		newGame = new Game();
 	}
+	
+
 	
 }
