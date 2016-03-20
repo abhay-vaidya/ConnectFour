b68dcd34caca67bbf161ad1ad1872aef361d4f43
Abhay Vaidya <abhay6547@gmail.com>
2016-03-20 18:32:53 -0400

    Cleaned up and added comments.

--- src/team/abhayumar/connect/Player.java
+++ src/team/abhayumar/connect/Player.java
@@ -8,11 +8,24 @@ public class Player {
 	private int losses = 0;
 	private int draws = 0;
 	
+	/**
+	 * Constructor with name and ID
+	 * @param name - Player name
+	 * @param id - Player ID (1 or 2)
+	 */
 	public Player(String name, int id) {
 		this.name = name;
 		this.id = id;
 	}
 	
+	/**
+	 * Constructor for possible file loading
+	 * @param name - Player name
+	 * @param id - Player ID (1 or 2)
+	 * @param wins - Player's wins
+	 * @param losses - Player's losses
+	 * @param draws - Player's draws
+	 */
 	public Player(String name, int id, int wins, int losses, int draws) {
 		this.name = name;
 		this.id = id;
@@ -22,6 +35,7 @@ public class Player {
 	}
 
 	/**
+	 * Returns player's name
 	 * @return - The player's name
 	 */
 	public String getName() {
@@ -29,6 +43,7 @@ public class Player {
 	}
 
 	/**
+	 * Sets player's name
 	 * @param name - The new name to set
 	 */
 	public void setName(String name) {
@@ -36,6 +51,7 @@ public class Player {
 	}
 	
 	/**
+	 * Gets player's ID
 	 * @return - The player's ID
 	 */
 	public int getID() {
@@ -43,6 +59,7 @@ public class Player {
 	}
 	
 	/**
+	 * Sets player's ID
 	 * @param id - The new ID to set
 	 */
 	public void setID(int id) {
@@ -50,13 +67,15 @@ public class Player {
 	}
 
 	/**
-	 * @return - The player's # of wins
+	 * Gets player's wins
+	 * @return wins - The player's # of wins
 	 */
 	public int getWins() {
 		return wins;
 	}
 
 	/**
+	 * Sets player's wins
 	 * @param wins - The new # of wins to set
 	 */
 	public void setWins(int wins) {
@@ -64,13 +83,15 @@ public class Player {
 	}
 
 	/**
-	 * @return - The player's # of losses
+	 * Gets player's losses
+	 * @return losses - The player's # of losses
 	 */
 	public int getLosses() {
 		return losses;
 	}
 
 	/**
+	 * Sets player's losses
 	 * @param losses - The new # of losses to set
 	 */
 	public void setLosses(int losses) {
@@ -78,25 +99,27 @@ public class Player {
 	}
 
 	/**
-	 * @return - The player's # of draws
+	 * Gets player's draws
+	 * @return draws - The player's # of draws
 	 */
 	public int getDraws() {
 		return draws;
 	}
 
 	/**
+	 * Sets player's draws
 	 * @param draws - The new # of draws to set
 	 */
 	public void setDraws(int draws) {
 		this.draws = draws;
 	}
 	
-	@Override
+	/**
+	 * Override equals method
+	 * @Override
+	 */
 	public boolean equals(Object obj) {
 		Player p = (Player)obj;
 		return p.getID() == this.getID();
 	}
-	
-	
-	
 }
