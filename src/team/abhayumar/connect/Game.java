2dbea3a000af2bb78d8e3eb5f925d0238b95a7e6
Abhay Vaidya <abhay6547@gmail.com>
2016-03-20 14:39:33 -0400

    Added rudimentary winner-checking algorithm.

--- src/team/abhayumar/connect/Game.java
+++ src/team/abhayumar/connect/Game.java
@@ -8,7 +8,7 @@ package team.abhayumar.connect;
 public class Game {
 	private Player playerOne;
 	private Player playerTwo;
-	public Player turn;
+	public Player turn = playerOne;
 	private int rows = 6;
 	private int columns = 7;
 	private int board[][] = new int[rows][columns];
@@ -48,8 +48,8 @@ public class Game {
 	 * Fills board array with zero/unoccupied values
 	 */
 	public void initializeBoard(){
-		for (int i=0; i < rows; i++){
-			for (int j=0; j < columns; j++){
+		for (int i = 0; i < rows; i++){
+			for (int j = 0; j < columns; j++){
 				board[i][j] = 0;
 			}
 		}
@@ -65,14 +65,57 @@ public class Game {
 		board[row][col] = player.getID();
 	}
 	
+	public void nextTurn(){
+		if (turn.equals(playerOne)){
+			turn = playerTwo;
+		}
+		else if (turn.equals(playerTwo)){
+			turn = playerOne;
+		}
+	}
+	
 	/**
 	 * Checks board 2D array for any winners
 	 * @return Boolean
 	 */
-	public boolean hasWinner(){
-		return false;
-		
+	public Player hasWinner(){
+		boolean winnerOne = false;
+		boolean winnerTwo = false;
+		for (int i = 0; i < rows; i ++) {
+			for (int j = 0; j < columns; j ++) {
+				// Check player one  
+		    	if (board[i][j] == 1 && j < 4 && i < 3){
+		    		// Check horizontal
+		    		if(board[i][j+1] == 1 && board[i][j+2] == 1 && board[i][j+3] == 1){
+		    			winnerOne = true;
+		    		}
+		    		// Check vertical
+		    		if(board[i+1][j] == 1 && board[i+2][j] == 1 && board[i+3][j] == 1){
+		    			winnerOne = true;
+		    		}
+		    	} 	
+				// Check player two  
+		    	if (board[i][j] == 2 && j < 4 && i < 3){
+		    		// Check horizontal
+		    		if(board[i][j+1] == 2 && board[i][j+2] == 2 && board[i][j+3] == 2){
+		    			winnerTwo = true;
+		    		}
+		    		// Check vertical
+		    		if(board[i+1][j] == 2 && board[i+2][j] == 2 && board[i+3][j] == 2){
+		    			winnerTwo = true;
+		    		}
+		    	} 
+		    }		
+		}
+		if (winnerOne == true){
+			return playerOne;
+		}
+		else if (winnerTwo == true){
+			return playerTwo;
+		}
+		else return null;
 	}
-	
-	
 }
+	
+	
+
