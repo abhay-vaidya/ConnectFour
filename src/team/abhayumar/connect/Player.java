9902513c0fd864d640d4ab6d5ea2df8c70a18af7
Umar Ahmed <umar.ahmed1998@gmail.com>
2016-03-20 13:18:33 -0400

    Implemented Player class.

--- src/team/abhayumar/connect/Player.java
+++ src/team/abhayumar/connect/Player.java
@@ -2,4 +2,79 @@ package team.abhayumar.connect;
 
 public class Player {
 
+	private String name;
+	private int wins = 0;
+	private int losses = 0;
+	private int draws = 0;
+	
+	public Player(String name) {
+		this.name = name;
+	}
+	
+	public Player(String name, int wins, int losses, int draws) {
+		this.name = name;
+		this.wins = wins;
+		this.losses = losses;
+		this.draws = draws;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the wins
+	 */
+	public int getWins() {
+		return wins;
+	}
+
+	/**
+	 * @param wins the wins to set
+	 */
+	public void setWins(int wins) {
+		this.wins = wins;
+	}
+
+	/**
+	 * @return the losses
+	 */
+	public int getLosses() {
+		return losses;
+	}
+
+	/**
+	 * @param losses the losses to set
+	 */
+	public void setLosses(int losses) {
+		this.losses = losses;
+	}
+
+	/**
+	 * @return the draws
+	 */
+	public int getDraws() {
+		return draws;
+	}
+
+	/**
+	 * @param draws the draws to set
+	 */
+	public void setDraws(int draws) {
+		this.draws = draws;
+	}
+	
+	
+	
+	
 }
