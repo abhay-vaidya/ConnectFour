62dc254dd7505068151e19ae68041f6b9b4e6d92
Abhay Vaidya <abhay6547@gmail.com>
2016-03-20 13:20:18 -0400

    Game class.

--- src/team/abhayumar/connect/Game.java
+++ src/team/abhayumar/connect/Game.java
@@ -1,5 +1,56 @@
 package team.abhayumar.connect;
 
+/**
+ * Class to handle game execution
+ * @author Abhay, Umar
+ * @version 1.0
+ */
 public class Game {
-
+	private Player playerOne;
+	private Player playerTwo;
+	public Player turn;
+	private int board[][];
+	
+	/**
+	 * Creates player object
+	 * @param name - Name of player
+	 */
+	public void createPlayer(int player, String name){
+		if (player == 1){
+			playerOne = new Player(name);
+		}
+		else {
+			playerTwo = new Player(name);
+		}
+	}
+	
+	/**
+	 * Runs game with two player objects
+	 * @param p1 - Player One
+	 * @param p2 - Player Two
+	 */
+	public void runGame(String p1, String p2){
+		createPlayer(1, p1);
+		createPlayer(2, p2);
+	}
+	
+	/**
+	 * Updates board 2D array
+	 * @param row - Row value
+	 * @param col - Column value
+	 */
+	public void updateBoard(int row, int col){
+		
+	}
+	
+	/**
+	 * Checks board 2D array for any winners
+	 * @return
+	 */
+	public boolean hasWinner(){
+		return false;
+		
+	}
+	
+	
 }
