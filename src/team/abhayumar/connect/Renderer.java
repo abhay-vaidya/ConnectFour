20ecd7ad9056c412e60fb0c27d6981c31b2d969b
323977454 <323977454@C-C80RCX1.tdsb.on.ca>
2016-03-24 13:41:18 -0400

    Added clear method.

--- src/team/abhayumar/connect/Renderer.java
+++ src/team/abhayumar/connect/Renderer.java
@@ -2,14 +2,20 @@ package team.abhayumar.connect;
 
 public class Renderer {
 	
+	private int width, height;
 	private int[] pixels;
 	
 	public Renderer() {
 		
 	}
 	
-	public Renderer(int width, int height) {
+	public Renderer(int w, int h) {
+		width = w;
+		height = h;
 		
+	}
+	
+	public clear() {
 		
 	}
 	
