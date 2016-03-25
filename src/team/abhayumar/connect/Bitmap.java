19305bf682d87a341fa91fdd8e478834b746b84d
Umar Ahmed <umar.ahmed1998@gmail.com>
2016-03-24 20:46:14 -0400

    Added renderer and modfied Bitmap class.

--- src/team/abhayumar/connect/Bitmap.java
+++ src/team/abhayumar/connect/Bitmap.java
@@ -14,15 +14,34 @@ public class Bitmap {
 	public Bitmap(int w, int h) {
 		width = w;
 		height = h;
-		pixels = new int[width * height];
+		setPixels(new int[width * height]);
 	}
 	
 	public void clear() {
-		Arrays.fill(pixels, 0);
+		Arrays.fill(getPixels(), 0);
 	}
 	
-	public void draw(Bitmap bmp, int x, int y) {
+	public void render(Bitmap bmp, int x, int y) {
+		int x1 = x;
+		int x2 = x + bmp.width;
+		int y1 = y;
+		int y2 = y + bmp.height;
 		
+		if (x1 < 0) x1 = 0;
+		if (x2 > bmp.width) x2 = bmp.width;
+		if (y1 < 0) y1 = 0;
+		if (y2 > bmp.height) y2 = bmp.height;
+		
+		
+		
+	}
+
+	public int[] getPixels() {
+		return pixels;
+	}
+
+	public void setPixels(int[] pixels) {
+		this.pixels = pixels;
 	}
 	
 }
---
+++ src/team/abhayumar/connect/Renderer.java
@@ -0,0 +1,16 @@
+package team.abhayumar.connect;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+public class Renderer extends Bitmap {
+
+	private BufferedImage image;
+	
+	public Renderer(int w, int h) {
+		super(w,h);
+		image = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+		setPixels(((DataBufferInt)image.getRaster().getDataBuffer()).getData());
+	}
+	
+}
