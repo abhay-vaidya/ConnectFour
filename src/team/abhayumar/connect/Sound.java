2ef7111573cd65433f5557854ab54c1acf4927c2
Abhay Vaidya <abhay6547@gmail.com>
2016-04-03 14:37:46 -0400

    Added/Cleaned up JavaDoc

--- src/team/abhayumar/connect/Sound.java
+++ src/team/abhayumar/connect/Sound.java
@@ -1,23 +1,35 @@
 package team.abhayumar.connect;
 
-import java.applet.AudioClip;
 import java.io.File;
 import java.io.IOException;
-
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+/**
+ * Class to handle game sound.
+ * 
+ * @author Abhay, Umar
+ * @version 1.0
+ */
 public class Sound implements Runnable {
 
+	// Initialize variables
 	private Clip clip;
 	private Thread thread;
 	private Float volume = -10.0f;
 
+	/**
+	 * Constructor to load sound clip.
+	 * 
+	 * @param path  the path to sound file
+	 * @param loop  whether the sound should loop or not
+	 */
 	public Sound(String path, boolean loop) {
-
+		
+		// Load clip
 		try {
 			clip = AudioSystem.getClip();
 			clip.open(AudioSystem.getAudioInputStream(new File(path)));
@@ -37,10 +49,14 @@ public class Sound implements Runnable {
 			e.printStackTrace();
 		}
 
+		// Create new thread
 		thread = new Thread(this);
 		thread.start();
 	}
 
+	/**
+	 * Toggles volume mute.
+	 */
 	public void toggleVolume() {
 		if (volume == -10.0f) {
 			volume = -80.0f;
@@ -51,7 +67,9 @@ public class Sound implements Runnable {
 		gainControl.setValue(volume);
 	}
 
-	@Override
+	/**
+	 * Runs sound thread.
+	 */
 	public void run() {
 		try {
 			clip.start();
