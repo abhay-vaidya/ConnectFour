f294c8ed091d4848bd1b25bbdd8b71086180004d
Abhay Vaidya <abhay6547@gmail.com>
2016-04-03 14:36:52 -0400

    Added/Cleaned up JavaDoc

--- src/team/abhayumar/connect/Screen.java
+++ src/team/abhayumar/connect/Screen.java
@@ -2,18 +2,27 @@ package team.abhayumar.connect;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
-
+/**
+ * Represents final screen to be displayed every time interval
+ * 
+ * @author Abhay, Umar
+ * @version 1.0
+ */
 public class Screen extends Bitmap {
-
-	public BufferedImage image;
 	
+	// Initialize variable
+	public BufferedImage image;
+
+	/**
+	 * Generates screen image.
+	 * 
+	 * @param w  width of image
+	 * @param h  height of image
+	 */
 	public Screen(int w, int h) {
-		super(w,h);
+		super(w, h);
 		image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
 		pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
-	}	
+	}
 }
